451dcb9b243ab96e5dfc79e369548caa02b3bc26
jaimeoliver1 <jaimeoliver1@gmail.com>
2018-05-16 10:50:48 +0200

    Add files via upload

---
+++ advanced material/haversine.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def haversine(lat1, lon1, lat2, lon2):
+    MILES = 3959
+    lat1, lon1, lat2, lon2 = map(np.deg2rad, [lat1, lon1, lat2, lon2])
+    dlat = lat2 - lat1 
+    dlon = lon2 - lon1 
+    a = np.sin(dlat/2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2)**2
+    c = 2 * np.arcsin(np.sqrt(a)) 
+    total_miles = MILES * c
+    return total_miles
+
